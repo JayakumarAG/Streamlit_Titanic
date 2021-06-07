762d8fb10e37e7980137f28e481548045b162ef5
JayakumarAG <85345788+JayakumarAG@users.noreply.github.com>
2021-06-07 23:00:22 +0530

    Update set up.sh

--- set up.sh
+++ set up.sh
@@ -1,4 +1,8 @@
-streamlit
-pandas
-numpy
-seaborn
+	      mkdir -p ~/.streamlit/
+	      echo "\
+              [server]\n\
+              headless = true\n\
+              port = $PORT\n\
+              enableCORS = false\n\
+              \n\
+              " > ~/.streamlit/config.toml
